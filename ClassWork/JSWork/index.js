07023017ebea418050bcd0433a6d8ed05deb519a
aryanrth <rathouraryan200@gamil.com>
2024-10-18 08:58:31 +0530

    javascript

---
+++ ClassWork/JSWork/index.js
@@ -0,0 +1,14 @@
+// console.log("hello");
+// const a=12;
+// if(a>10){
+//   let b=23;
+//   a=70;
+//   console.log("a="+a);
+// }
+// console.log("hi,a="+b);
+function fun(a,b){
+  console.log("hee");
+  return a+b;
+}
+let d=fun(2,6);
+console.log(d);
