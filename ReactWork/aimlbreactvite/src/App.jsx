e6054d153bf1b3c0346b719469218bd1b781aba3
aryanrth <rathouraryan200@gmail.com>
2024-11-19 10:27:49 +0530

    react work

--- ReactWork/aimlbreactvite/src/App.jsx
+++ ReactWork/aimlbreactvite/src/App.jsx
@@ -1,17 +1,59 @@
 import React from "react";
+import Student from "./student";
+import Pic from "./Pic.jpg";
 
 function App() {
-  let a = 20;
-  let mystyle = {
-    backgroundColor: "cyan",
-    color: "red",
-    border:"2px solid black"
-  };
+  const Studentdata={
+    college:"ABES ENGINEERING",
+    Pic:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+    Roll: "47",
+    Branch:"CSEAIML",
+    Name:"Aryan Kumar Rathore",
+    section:"B"
+   
+  }
+  // let a = 20;
+  // let mystyle = {
+  //   backgroundColor: "cyan",
+  //   color: "red",
+  //   border: "2px solid black",
+  // };
   return (
-    <div style={{ color: "red" }}>
-      <h2>Apna college</h2>
-      <div style={mystyle}>{a}</div>
+    // <div style={{ color: "red" }}>
+    //   <h2>Apna college</h2>
+    //   <div style={mystyle}>{a}</div>
+    <div style={{marginLeft:"200px"}}>
+    <Student data={Studentdata}/>
+    <div>
+      {/* <Student
+        college="ABES Engineering college"
+        Pic={<img src={Pic} height={80} width={80}></img>}
+        Roll="47"
+        Branch="CSEAIM"
+        Name="Aryan Kumar Rathore"
+        section="B"
+        
+      ></Student>
+        <Student
+        college="ABES Engineering college"
+        Pic={<img src={Pic} height={80} width={80}></img>}
+        Roll="47"
+        Branch="CSEAIM"
+        Name="Aryan Kumar Rathore"
+        section="B"
+        
+      ></Student>
+        <Student
+        college="ABES Engineering college"
+        Pic={<img src={Pic} height={80} width={100}></img>}
+        Roll="47"
+        Branch="CSEAIM"
+        Name="Aryan Kumar Rathore"
+        section="B"
+        
+      ></Student> */}
     </div>
+     </div>
   );
 }
 
---
+++ ReactWork/aimlbreactvite/src/Student.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./StudentStyle.css";
+
+function Student({ data }) {
+  return (
+    <div>
+  
+      {<table>
+        <tbody>
+          <tr>
+            <td colSpan={2}>{data.colllege}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}><img src={data.Pic} height={80} width={80}></img></td>
+          </tr>
+          <tr>
+            <td>Roll:</td>
+            <td>{data.Roll}</td>
+          </tr>
+          <tr>
+            <td>Branch:</td>
+            <td>{data.Branch}</td>
+          </tr>
+          <tr>
+            <td>Name:</td>
+            <td>{data.Name}</td>
+          </tr>
+          <tr>
+            <td>section:</td>
+            <td>{data.section}</td>
+          </tr>
+        </tbody>
+      </table> }
+    </div>
+  );
+}
+Student.defaultdata = {
+  colllege: "AKG Engineering college",
+};
+
+export default Student;
